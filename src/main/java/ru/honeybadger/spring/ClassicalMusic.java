ce36fdf234cc839290eae22d426825319b394339
22Dimas54 <altaidimas@mail.ru>
2020-02-17 22:46:11 +0700

    Аннотации @Scope, @Value, @PostConstruct, @PreDestroy

--- src/main/java/ru/honeybadger/spring/ClassicalMusic.java
+++ src/main/java/ru/honeybadger/spring/ClassicalMusic.java
@@ -1,14 +1,27 @@
 package ru.honeybadger.spring;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+//@Scope("prototype")
 public class ClassicalMusic implements Music {
-    private List<String> songs = new ArrayList<>();
 
+    private List<String> songs = new ArrayList<>();
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     // Блок инициализации объекта (англ. Instance initialization block)
     // Выполняется каждый раз, когда создается объект класса
     {
--- src/main/java/ru/honeybadger/spring/TestSpring.java
+++ src/main/java/ru/honeybadger/spring/TestSpring.java
@@ -14,6 +14,14 @@ public class TestSpring {
         musicPlayer.playMusic(GenerOfMusic.CLASSICAL);
         musicPlayer.playMusic(GenerOfMusic.ROCK);
 
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic",ClassicalMusic.class);
+        //ClassicalMusic classicalMusic2 = context.getBean("classicalMusic",ClassicalMusic.class);
+
+        //System.out.println(classicalMusic1==classicalMusic2);
+
         context.close();
     }
 }
